35ee81648d0cae29b13b2b62990fecf4ffc43220
omar gallo <omargallob@gmail.com>
2012-02-05 13:26:14 +0100

    change carrierwave settings

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,7 @@
 CarrierWave.configure do |config|
-  config.permissions = 0666
+  config.permissions = 0777
   config.storage = :s3
-  config.root = Rails.root.join('tmp')
-  config.cache_dir = 'carrierwave'
+
 
   config.s3_access_key_id = "AKIAI7DG2I3UGHEC6FKA"
   config.s3_secret_access_key = "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"
